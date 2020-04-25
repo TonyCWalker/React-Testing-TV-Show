485143a6c68c98002efd35a5c09f24504fed9acf
Tony Walker <Walkertony509@gmail.com>
2020-04-24 18:20:51 -0700

    Initial set up.

---
+++ src/api/fetchshow.js
@@ -0,0 +1,12 @@
+export const fetchShow = () => {
+    return axios.get
+      .then(res => return res) // or res.data, however you want to set that up
+  }
+    
+   // App.js
+  useEffect(() => {
+    fetchShow
+      .then(res => {
+        // set state with the data
+      }
+  }, []);
