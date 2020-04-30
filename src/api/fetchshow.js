7689f42d03365f848f547d111e9f74af18c1533e
Tony Walker <Walkertony509@gmail.com>
2020-04-29 19:41:26 -0700

    MVP

--- src/api/fetchshow.js
+++ src/api/fetchshow.js
@@ -1,12 +1,15 @@
+import React from 'react';
+import axios from 'axios'
+
 export const fetchShow = () => {
-    return axios.get
-      .then(res => return res) // or res.data, however you want to set that up
-  }
-    
-   // App.js
-  useEffect(() => {
-    fetchShow
+    return axios
+      .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(res => {
-        // set state with the data
-      }
-  }, []);
+          console.log(res);
+          return res;
+      })
+      .catch(error => {
+          console.log(error)
+          return error;
+      })
+  };
